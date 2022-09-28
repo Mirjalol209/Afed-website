1622800322bcc51a4e5f0ef95b7b29dbdc71ba24
Mirjalol <urakovmirjalol209@gmail.com>
2022-09-28 12:40:13 +0100

    feat: routing sent

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.css'
+function Home (){
+    return(
+        <>
+       <div className="home__banner">
+        <div className="banner__title">
+            <h1>Fashion passes, style remains.</h1>
+      <p>To enter into good society, one must wear a tailcoat, uniform or livery.</p>
+            
+        </div>
+       </div>
+        </>
+    )
+}
+export default Home;
