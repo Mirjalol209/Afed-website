6d315ca400d8f00f5fa7fc1980eb29b180018583
Mirjalol <urakovmirjalol209@gmail.com>
2022-09-30 18:17:56 +0100

    fixing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Offer from "./pages/Offer/Offer";
+import Contact from "./pages/Contact/Contact";
+import Primelist from "./pages/Primelist/Primelist"
+
 function App() {
   return (
     <div className="App">
