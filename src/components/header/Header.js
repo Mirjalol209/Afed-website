d3a196c6401c7be3e70baca10ada56fa10bf560f
Umar <umaribragimovich555@gmail.com>
2022-09-30 15:59:27 +0500

    feat: header-menu-responsive has done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { BsTelephone } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import headerLogo from "../../assets/images/header-logo.svg";
+import headerLogo1 from "../../assets/images/header/header-logo-lg.svg";
+import headerLogo2 from "../../assets/images/header/header-logo-sm.svg";
 
 function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
     <div className="header">
       <div className="header__left">
         <div className="header__logo">
-          <img className="logo" src={headerLogo} alt="/" />
+          <picture>
+            <source srcset={headerLogo2} media="(max-width: 400px)" />
+            <img className="logo" src={headerLogo1} alt="/" />
+          </picture>
+
+          <div className="header__menu__responsive">
+            {openMenu ? (
+              <div className="header__routing__responsive">
+                <Link to={"/"}>Home</Link>
+                <Link to={"/about"}>About</Link>
+                <Link to={"/offer"}>Offer</Link>
+                <Link to={"/primelist"}>Primelist</Link>
+                <Link to={"/contact"}>Contacts</Link>
+              </div>
+            ) : null}
+            <button className="header__button__responsive" onClick={toggleMenu}>
+              <GiHamburgerMenu />
+            </button>
+          </div>
         </div>
+
         <div className="header__menu">
           <Link to={"/"}>Home</Link>
           <Link to={"/about"}>About</Link>
