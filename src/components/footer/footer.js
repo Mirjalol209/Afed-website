08d57720dc9b60b576bdf08e32ecfb93eee7ccf4
Mirjalol <urakovmirjalol209@gmail.com>
2022-10-01 15:23:56 +0100

    feat:Refinements footer-component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,7 +12,7 @@ function Footer (){
     <footer>
     <div className="footer__section">
    
-     <div className="footer__logo">    <img className="logo" src={footerLogo1} alt="/" />
+     <div className="footer__logo"> <Link to={"/"}> <img className="logo" src={footerLogo1} alt="/" /></Link>  
 </div>
       <div className="footer__socialNetwork"><AiOutlineInstagram size={'30px'} color={'#c5bf9f'}/> <FaTelegramPlane size={'30px'} color={'#c5bf9f'}/> <AiOutlineFacebook size={'30px'} color={'#c5bf9f'}/> <AiOutlineTwitter size={'30px'} color={'#c5bf9f'}/></div>
         <div className="footer__info"><p>all rights reserved.</p>
